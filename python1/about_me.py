9c890e305c46d1882b15b4934ba101d644a4bc4a
pfb2024 <pfb2024@info24.cshl.edu>
2024-10-15 11:48:49 -0400

    adding python1

---
+++ python1/about_me.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+name = sys.argv[1]
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+print(name, 'likes', color, 'and', activity, 'and', animal)
+
+print(f"My name: {name}")
+print("My name: chan young")
+print("My favorite color : green")
+print("My favorite activity : Coding")
+print("My favorite animal : chicken")
